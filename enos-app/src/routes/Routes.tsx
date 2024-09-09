c7b5d0eb0ac47a3010acba8e2aa3ea5979e75688
Enes Önal <enes.onal777@gmail.com>
2024-09-09 17:22:25 +0300

    set initial routes

---
+++ enos-app/src/routes/Routes.tsx
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from "react";
+import { Navigate, Routes as ReactRouterRoutes, Route } from "react-router-dom";
+
+export function Routes() {
+    const routeConfig = [
+        {
+          path: "/",
+          component: lazy(() => import("../App")),
+        }
+      ];
+
+  return (
+    <ReactRouterRoutes>
+      <Route>
+        {routeConfig.map(({ path, component: Component }) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <Suspense>
+                <Component />
+              </Suspense>
+            }
+          />
+        ))}
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </ReactRouterRoutes>
+  );
+}
