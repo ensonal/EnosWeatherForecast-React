af50b4478a4dae6b70ba1dd33d00a76efc63922a
Enes Önal <enes.onal777@gmail.com>
2024-09-10 23:21:04 +0300

    refact: get table data and use it

--- enos-app/src/modules/forecast/ui/ForecastTableContainer/ForecastTableContainer.tsx
+++ enos-app/src/modules/forecast/ui/ForecastTableContainer/ForecastTableContainer.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import DataTable from "../../../../components/common/DataTable/DataTable";
+import { useWeatherContext } from "../../hooks/useWeatherContext";
 
 export function ForecastTableContainer() {
+    const { weatherData, loading, error } = useWeatherContext();
+
+    useEffect(() => {
+        console.log(weatherData);
+    });
+
     const columns = [
         { header: "Days", accessor: "day" },
         { header: "Dates", accessor: "date" },
@@ -8,19 +16,16 @@ export function ForecastTableContainer() {
         { header: "Highest Temp.", accessor: "high" }
     ];
 
-    const forecastData = [
-        { day: "Monday", date: "August 26, 2024", low: "24°C", high: "30°C" },
-        { day: "Tuesday", date: "August 27, 2024", low: "26°C", high: "31°C" },
-        { day: "Wednesday", date: "August 28, 2024", low: "25°C", high: "32°C" },
-        { day: "Thursday", date: "August 29, 2024", low: "24°C", high: "34°C" },
-        { day: "Friday", date: "August 30, 2024", low: "25°C", high: "33°C" },
-        { day: "Saturday", date: "August 31, 2024", low: "26°C", high: "33°C" },
-        { day: "Sunday", date: "September 1, 2024", low: "23°C", high: "31°C" }
-    ];
+    const forecastData = weatherData?.map((day) => ({
+        day: day.day,
+        date: day.date,
+        low: day.lowestTemp,
+        high: day.highestTemp
+    }));
 
     return (
-        <div>
-            <DataTable title="Weather Forecast for Ankara" columns={columns} data={forecastData} />
+        <div className="w-100">
+            <DataTable title="Weather Forecast for Ankara" columns={columns} data={forecastData ? forecastData : []} />
         </div>
     );
 }
