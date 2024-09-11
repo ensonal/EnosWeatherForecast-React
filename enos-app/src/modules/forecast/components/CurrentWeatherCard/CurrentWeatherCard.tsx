71383e96c9429e6319ca151b7e70f724def7313d
Enes Önal <enes.onal777@gmail.com>
2024-09-11 22:09:25 +0300

    complete loading logic for weathercard

--- enos-app/src/modules/forecast/components/CurrentWeatherCard/CurrentWeatherCard.tsx
+++ enos-app/src/modules/forecast/components/CurrentWeatherCard/CurrentWeatherCard.tsx
@@ -3,12 +3,18 @@ import c04d from "../../../../assets/icons/weather/c04d.png";
 import { useWeatherContext } from "../../hooks/useWeatherContext";
 
 export function CurrentWeatherCard() {
-    const { selectedWeatherData } = useWeatherContext();
+    const { selectedWeatherData, loading } = useWeatherContext();
 
     const formattedDate = selectedWeatherData?.date.split(",")[0] + ", " + selectedWeatherData?.day;
-    const iconPath = selectedWeatherData ? require(`../../../../assets/icons/weather/${selectedWeatherData.weather.icon}.png`) : c04d;
+    const iconPath = selectedWeatherData
+        ? require(`../../../../assets/icons/weather/${selectedWeatherData.weather.icon}.png`)
+        : c04d;
 
-    return (
+    return loading ? (
+        <div className="default-card current-weather-card d-flex flex-column align-items-center">
+            <div className="spinner-grow" role="status" />
+        </div>
+    ) : (
         <div className="default-card current-weather-card d-flex flex-column align-items-center justify-content-between">
             <p className="degree-text m-0">{selectedWeatherData?.averageTemp}</p>
             <div className="general-info d-flex flex-column align-items-center">
