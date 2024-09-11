d78f0c15e97dd90c998e330c3ae55f4731f5c1ec
Enes Önal <enes.onal777@gmail.com>
2024-09-11 12:38:30 +0300

    missing empty state logic

--- enos-app/src/modules/forecast/ui/SearchSummaryContainer/SearchSummaryContainer.tsx
+++ enos-app/src/modules/forecast/ui/SearchSummaryContainer/SearchSummaryContainer.tsx
@@ -2,14 +2,17 @@ import { SearchInputField } from "../../../../components/common/SearchInputField
 import { CurrentWeatherCard } from "../../components/CurrentWeatherCard/CurrentWeatherCard";
 import DisplayEmptyStateCard from "../../components/DisplayEmptyStateCard/DisplayEmptyStateCard";
 import SelectInfoCard from "../../components/SelectInfoCard/SelectInfoCard";
+import { useWeatherContext } from "../../hooks/useWeatherContext";
 
 export function SearchSummaryContainer() {
+    const { weatherData, cache } = useWeatherContext();
+
     return (
         <div className="d-flex flex-column gap-3">
             <SearchInputField />
-            <CurrentWeatherCard />
-            <SelectInfoCard />
             <DisplayEmptyStateCard />
+            {weatherData.length > 0 ? <CurrentWeatherCard /> : null}
+            {Object.keys(cache).length === 0 && weatherData.length === 0 ? <SelectInfoCard /> : null}
         </div>
     );
 }
