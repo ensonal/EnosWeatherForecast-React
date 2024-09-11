57c9a677e222b97d1c00ce191f1f204950ac9f28
Enes Önal <enes.onal777@gmail.com>
2024-09-12 01:43:15 +0300

    add size provider to app

--- enos-app/src/App.tsx
+++ enos-app/src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./routes/Routes";
 import "./App.css";
 import { NavBar } from "./components/NavBar/NavBar";
+import { ScreenSizeProvider } from "./providers/ScreenSizeProvider";
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-          <NavBar />
-          <Routes />
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ScreenSizeProvider>
+                <Router>
+                    <NavBar />
+                    <Routes />
+                </Router>
+            </ScreenSizeProvider>
+        </div>
+    );
 }
 
 export default App;
